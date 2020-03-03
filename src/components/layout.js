6b1ccacf70f2db52cced2321ab1af8eb23c0f58c
McHo Zhang <>
2020-03-03 21:38:00 +1100

    page

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import content from "../resume.yaml"
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  console.log(content);
 
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
